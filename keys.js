c33551a8c81e14f93787decab6078eed77fb45f2
kellyjoey <kellyjoey@me.com>
2017-02-04 23:06:33 -0500

    Feb 4 work

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '0OQ7zd0b6ifOzShQc0yFelPTR',
+  consumer_secret: 'OuK4Ons9jOQ8vcM7BG5SOlj9qoDLuzFLbvKiSt4AgohH4cBaP7',
+  access_token_key: '124198720-k1ua3PunNlXBBLZPU5DZYT4Ypby3yAOfjPqKQooj',
+  access_token_secret: '6y0Su4lHpYMpEyaBE91WvevPpZQJYV0FTcEvO5GJfJX7k',
+}
